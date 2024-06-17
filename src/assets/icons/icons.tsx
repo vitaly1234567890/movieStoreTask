d948cf5b596ab5c36fe1fb3f81367c51ed1aee0b
vitaly1234567890 <Troyanvitaly123@yandex.ru>
2024-06-17 10:58:00 +0300

    fix component 404

--- src/assets/icons/icons.tsx
+++ src/assets/icons/icons.tsx
@@ -1,40 +1,40 @@
-import { forwardRef, memo } from 'react'
+import { forwardRef, memo } from "react";
 
-import IconsSprite from './iconsSprite.svg'
+import IconsSprite from "./iconsSprite.svg";
 
 export type IconsPropsType = {
-    className?: string
-    full?: string
-    height?: string
-    iconId: string
-    viewBox?: string
-    width?: string
-}
+  className?: string;
+  full?: string;
+  height?: string;
+  iconId: string;
+  viewBox?: string;
+  width?: string;
+};
 export const Icons = memo(
-    forwardRef<SVGSVGElement, IconsPropsType>((props, ref) => {
-        const {
-            className,
-            full = 'none',
-            height = '32',
-            iconId,
-            viewBox = '0 0 32 32',
-            width = '32',
-            ...rest
-        } = props
+  forwardRef<SVGSVGElement, IconsPropsType>((props, ref) => {
+    const {
+      className,
+      full = "none",
+      height = "32",
+      iconId,
+      viewBox = "0 0 32 32",
+      width = "32",
+      ...rest
+    } = props;
 
-        return (
-            <svg
-                className={className}
-                fill={full}
-                height={height}
-                ref={ref}
-                viewBox={viewBox}
-                width={width}
-                xmlns={'http://www.w3.org/2000/svg'}
-                {...rest}
-            >
-                <use xlinkHref={`${IconsSprite}#${iconId}`} />
-            </svg>
-        )
-    })
-)
+    return (
+      <svg
+        className={className}
+        fill={full}
+        height={height}
+        ref={ref}
+        viewBox={viewBox}
+        width={width}
+        xmlns={"http://www.w3.org/2000/svg"}
+        {...rest}
+      >
+        <use xlinkHref={`${IconsSprite}#${iconId}`} />
+      </svg>
+    );
+  }),
+);
--- src/components/404/404.tsx
+++ src/components/404/404.tsx
@@ -1,27 +1,31 @@
-import {Icons} from "../../assets/icons/icons.tsx";
-import s from './404.module.scss'
-import {Button} from "../ui/button/button.tsx";
-import {useNavigate} from "react-router-dom";
-
+import { Icons } from "../../assets/icons/icons.tsx";
+import s from "./404.module.scss";
+import { Button } from "../ui/button/button.tsx";
+import { useNavigate } from "react-router-dom";
 
 export const NotFound = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const onClickGoHome = () => {
-        navigate('/movies');
-    };
+  const onClickGoHome = () => {
+    navigate("/movies");
+  };
 
-    return (
-        <div className={s.root}>
-                <div className={s.icon}>
-                    <Icons iconId={"sidebar"}/>
-                    <span className={s.sidebarTitle}>ArrowFlicks</span>
-                </div>
-            <div className={s.content}>
-                <Icons viewBox={"0 0 656 196"} height={"196"} width={"656"} iconId={"404"}/>
-                <p className={s.title}>We can’t find the page you are looking for</p>
-                <Button children={"Go Home"} onClick={onClickGoHome}/>
-            </div>
-        </div>
-    );
+  return (
+    <div className={s.root}>
+      <div className={s.icon}>
+        <Icons iconId={"sidebar"} />
+        <span className={s.sidebarTitle}>ArrowFlicks</span>
+      </div>
+      <div className={s.content}>
+        <Icons
+          viewBox={"0 0 656 196"}
+          height={"196"}
+          width={"656"}
+          iconId={"404"}
+        />
+        <p className={s.title}>We can’t find the page you are looking for</p>
+        <Button children={"Go Home"} onClick={onClickGoHome} />
+      </div>
+    </div>
+  );
 };
